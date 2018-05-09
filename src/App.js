06dce1c91bbac287df720133baf72893624fe62d
DESKTOP-HASSIN\Hassin <hassin@cherrysoft.co.kr>
2018-05-09 18:22:23 +0900

    first commit for study react_movie_list

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+import Movie from './Movie';
+
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Start Wars"
+]
+
+const movieImages =[
+  "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+  "https://lumiere-a.akamaihd.net/v1/images/2-1_1000-x-500-px-tlj_979cf341.jpeg?region=0%2C0%2C1000%2C500&width=320"
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+      </div>
+    );
+  }
+}
+
+export default App;
